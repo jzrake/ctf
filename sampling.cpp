0a793d33994d6aeb2e15d3f8c9be44b762ae5808
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-02-03 19:46:34 -0500

    Moved bilinear interp into a function, trilinear interp function is a shell, thinking about how to write a test for it.

--- sampling.cpp
+++ sampling.cpp
@@ -9,12 +9,13 @@
 #include "hydro.hpp"
 #include "mara_mpi.h"
 
+static std::valarray<double>  bilinear_interp(const double *r);
+static std::valarray<double> trilinear_interp(const double *r);
 
 
 void Mara_prim_at_point(const double *r0, double *P1)
 {
   const PhysicalDomain &domain = *HydroModule::Mara->domain;
-
 
   const int rank = domain.SubgridRank();
   const int size = domain.SubgridSize();
@@ -73,7 +74,6 @@ void Mara_prim_at_point(const double *r0, double *P1)
   // to be in the correct place when the matching receive is posted.
   // ---------------------------------------------------------------------------
   std::valarray<double> *Pvector = new std::valarray<double>[queries_to_answer];
-  ValarrayManager M(domain.aug_shape(), Nq);
 
 
   for (int n=0; n<queries_to_answer; ++n) {
@@ -81,34 +81,14 @@ void Mara_prim_at_point(const double *r0, double *P1)
     double r_query[3];
     MPI_Recv(r_query, Nd, MPI_DOUBLE, MPI_ANY_SOURCE, tag, comm, &status);
 
-    const int i = (Nd>=1) ? domain.IndexAtPosition(r_query, 0) : 0;
-    const int j = (Nd>=2) ? domain.IndexAtPosition(r_query, 1) : 0;
-    const int k = (Nd>=3) ? domain.IndexAtPosition(r_query, 2) : 0;
-
     std::valarray<double> &Panswer = Pvector[n];
     Panswer.resize(Nq);
 
-    //    if (Nd == 1) Panswer = HydroModule::Mara->PrimitiveArray[M(i)];
-    //    if (Nd == 2) Panswer = HydroModule::Mara->PrimitiveArray[M(i,j)];
-    //    if (Nd == 3) Panswer = HydroModule::Mara->PrimitiveArray[M(i,j,k)];
-
-    std::valarray<double> P00 = HydroModule::Mara->PrimitiveArray[M(i-1,j-1)];
-    std::valarray<double> P01 = HydroModule::Mara->PrimitiveArray[M(i-1,j+1)];
-    std::valarray<double> P10 = HydroModule::Mara->PrimitiveArray[M(i+1,j-1)];
-    std::valarray<double> P11 = HydroModule::Mara->PrimitiveArray[M(i+1,j+1)];
-
-    double delx[3];
-
-    delx[0] = 0.5 * (r_query[0] - domain.x_at(i-1)) / domain.get_dx(1);
-    delx[1] = 0.5 * (r_query[1] - domain.y_at(j-1)) / domain.get_dx(2);
-
-    for (int q=0; q<Nq; ++q) {
-      double b1 = P00[q];
-      double b2 = P10[q] - P00[q];
-      double b3 = P01[q] - P00[q];
-      double b4 = P00[q] - P10[q] - P01[q] + P11[q];
-
-      Panswer[q] = b1 + b2*delx[0] + b3*delx[1] + b4*delx[0]*delx[1];
+    if (Nd == 2) {
+      Panswer = bilinear_interp(r_query);
+    }
+    else if (Nd == 1) {
+      Panswer = trilinear_interp(r_query);
     }
 
     MPI_Isend(&Panswer[0], Nq, MPI_DOUBLE, status.MPI_SOURCE, tag+1, comm,
@@ -126,7 +106,46 @@ void Mara_prim_at_point(const double *r0, double *P1)
 }
 
 
+std::valarray<double> bilinear_interp(const double *r)
+{
+  const PhysicalDomain &domain = *HydroModule::Mara->domain;
+  const int Nq = domain.get_Nq();
+  ValarrayManager M(domain.aug_shape(), Nq);
+  std::valarray<double> Panswer(Nq);
 
+  const int i = domain.IndexAtPosition(r, 0);
+  const int j = domain.IndexAtPosition(r, 1);
+
+  std::valarray<double> P00 = HydroModule::Mara->PrimitiveArray[M(i-1,j-1)];
+  std::valarray<double> P01 = HydroModule::Mara->PrimitiveArray[M(i-1,j+1)];
+  std::valarray<double> P10 = HydroModule::Mara->PrimitiveArray[M(i+1,j-1)];
+  std::valarray<double> P11 = HydroModule::Mara->PrimitiveArray[M(i+1,j+1)];
+
+  double delx[2];
+
+  delx[0] = 0.5 * (r[0] - domain.x_at(i-1)) / domain.get_dx(1);
+  delx[1] = 0.5 * (r[1] - domain.y_at(j-1)) / domain.get_dx(2);
+
+  for (int q=0; q<Nq; ++q) {
+    double b1 = P00[q];
+    double b2 = P10[q] - P00[q];
+    double b3 = P01[q] - P00[q];
+    double b4 = P00[q] - P10[q] - P01[q] + P11[q];
+
+    Panswer[q] = b1 + b2*delx[0] + b3*delx[1] + b4*delx[0]*delx[1];
+  }
+  return Panswer;
+}
+
+std::valarray<double> trilinear_interp(const double *r)
+{
+  const PhysicalDomain &domain = *HydroModule::Mara->domain;
+  const int Nq = domain.get_Nq();
+  ValarrayManager M(domain.aug_shape(), Nq);
+  std::valarray<double> Panswer(Nq);
+
+  return Panswer;
+}
 
 #else
 void Mara_prim_at_point(const double *r0, double *P1) { }
