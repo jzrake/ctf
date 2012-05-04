30a6fe0fe2668d0023bb0e12850fe8f606b912cc
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-05-04 14:27:52 -0400

    Put in alternative WENO weights, noticed that old weno-split integrator was actually fifth order

--- weno5-riemann.cpp
+++ weno5-riemann.cpp
@@ -11,9 +11,10 @@
 #include "plm-split.hpp"
 
 #define MAXNQ 8 // Used for static array initialization
+#define SQU(x) ((x)*(x))
+
 
 typedef Weno5RiemannMethodOfLinesSplit Deriv;
-
 
 static const double CeesA2C[3][3] = { {23./24.,  1./12.,  -1./24.},
                                       {-1./24., 13./12.,  -1./24.},
@@ -28,46 +29,51 @@ static const double CeesC2R[3][3] = { { 3./8., 3./4.,  -1./8.},
                                       {-1./8., 3./4.,   3./8.},
                                       { 3./8.,-5./4.,  15./8.} };
 
+static const double CeesC2R_A[3][3] = { { 1./3.,  5./6., -1./6. },
+					{-1./6.,  5./6.,  1./3. },
+					{ 1./3., -7./6., 11./6. } };
+static const double CeesC2L_A[3][3] = { {11./6., -7./6.,  1./3. },
+					{ 1./3.,  5./6., -1./6. },
+					{-1./6.,  5./6.,  1./3. } };
+
 static const double DeesC2L[3] = {   1./ 16.,   5./  8.,   5./ 16. };
 static const double DeesC2R[3] = {   5./ 16.,   5./  8.,   1./ 16. };
 static const double DeesA2C[3] = {  -9./ 80.,  49./ 40.,  -9./ 80. };
 static const double DeesC2A[3] = { -17./240., 137./120., -17./240. };
 
+static const double DeesC2L_A[3] = { 0.1, 0.6, 3.0 };
+static const double DeesC2R_A[3] = { 0.3, 0.6, 1.0 };
+
 
 static inline double weno5(const double *v, const double c[3][3], const double d[3])
 {
-  const double eps = 1e-5; // 1e-6 -> 1e-4
-  const int k = 3;
-  double w[3], vs[3];
+  const double eps = 1e-16; // 1e-6 -> 1e-4
 
   const double B[3] = {
-    (13.0/12.0)*pow(  v[ 0] - 2*v[ 1] +   v[ 2], 2.0) +
-    ( 1.0/ 4.0)*pow(3*v[ 0] - 4*v[ 1] +   v[ 2], 2.0),
+    (13.0/12.0)*SQU(  v[ 0] - 2*v[ 1] +   v[ 2]) +
+    ( 1.0/ 4.0)*SQU(3*v[ 0] - 4*v[ 1] +   v[ 2]),
 
-    (13.0/12.0)*pow(  v[-1] - 2*v[ 0] +   v[ 1], 2.0) +
-    ( 1.0/ 4.0)*pow(  v[-1] - 0*v[ 0] -   v[ 1], 2.0),
+    (13.0/12.0)*SQU(  v[-1] - 2*v[ 0] +   v[ 1]) +
+    ( 1.0/ 4.0)*SQU(  v[-1] - 0*v[ 0] -   v[ 1]),
 
-    (13.0/12.0)*pow(  v[-2] - 2*v[-1] +   v[ 0], 2.0) +
-    ( 1.0/ 4.0)*pow(  v[-2] - 4*v[-1] + 3*v[ 0], 2.0)
+    (13.0/12.0)*SQU(  v[-2] - 2*v[-1] +   v[ 0]) +
+    ( 1.0/ 4.0)*SQU(  v[-2] - 4*v[-1] + 3*v[ 0])
   };
 
-  double wtot = 0.0;
-  for (int r=0; r<k; ++r) {
-    vs[r] = 0.0;
-    for (int j=0; j<k; ++j) {
-      vs[r] += c[r][j] * v[j-r];
-    }
-    w[r] = d[r] / pow(eps + B[r], 2);
-    wtot += w[r];
-  }
+  const double vs[3] = {
+    c[0][0]*v[ 0] + c[0][1]*v[ 1] + c[0][2]*v[2],
+    c[1][0]*v[-1] + c[1][1]*v[ 0] + c[1][2]*v[1],
+    c[2][0]*v[-2] + c[2][1]*v[-1] + c[2][2]*v[0],
+  };
+  const double w[3] = {
+    d[0] / SQU(eps + B[0]),
+    d[1] / SQU(eps + B[1]),
+    d[2] / SQU(eps + B[2])
+  };
 
-  double viph = 0.0;
-  for (int r=0; r<k; ++r) {
-    viph += (w[r] / wtot) * vs[r];
-  }
-  return viph;
+  const double wtot = w[0] + w[1] + w[2];
+  return (w[0]*vs[0] + w[1]*vs[1] + w[2]*vs[2])/wtot;
 }
-
 
 std::valarray<double> Deriv::dUdt(const std::valarray<double> &Uin)
 {
@@ -104,6 +110,7 @@ void Deriv::intercell_flux_sweep(const double *U_avg, double *F, int dim)
 
   std::valarray<double> G(stride[0]); // is the point-valued HLL flux at i+1/2
   std::valarray<double> U(stride[0]); // point-valued conserved state for this axis
+  std::valarray<double> u(stride[0]);
   std::valarray<double> P = Mara->PrimitiveArray;
 
 
@@ -125,15 +132,11 @@ void Deriv::intercell_flux_sweep(const double *U_avg, double *F, int dim)
       exit(2);
     }
   }
-
   for (i=4*S; i<stride[0]-5*S; i+=NQ) {
     for (int q=0; q<NQ; ++q) {
       const int m = i + q;
       double vm[5] = { P[m-2*S], P[m-S], P[m+0], P[m+1*S], P[m+2*S] };
       double vp[5] = { P[m-1*S], P[m-0], P[m+S], P[m+2*S], P[m+3*S] };
-
-      //      Pl[q] = P[m];
-      //      Pr[q] = P[m+S];
 
       Pl[q] = weno5(vm+2, CeesC2R, DeesC2R);
       Pr[q] = weno5(vp+2, CeesC2L, DeesC2L);
