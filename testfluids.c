1cbeb532b555c505350612aae29fc13ec525dd56
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-07-30 03:38:21 -0400

    Tests the HLL and exact Riemann solvers

--- testfluids.c
+++ testfluids.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
+#define FLUIDS_PRIVATE_DEFS
 #include "fluids.h"
 #include "matrix.h"
+
+#define asserteq(x,y) assert(fabs(x-y) < 1e-12)
 
 // Passes when get/set attributes work correctly
 // -----------------------------------------------------------------------------
@@ -20,9 +23,9 @@ int test1()
   fluids_setattrib(S, x, FLUIDS_PRIMITIVE);
   fluids_getattrib(S, y, FLUIDS_PRIMITIVE);
   fluids_del(S);
-  assert(Gam == gam);
+  asserteq(Gam, gam);
   for (int n=0; n<5; ++n) {
-    assert(y[n] == 1.0);
+    asserteq(y[n], 1.0);
   }
   printf("TEST 1 PASSED\n");
   return 0;
@@ -47,13 +50,13 @@ int test2()
   fluids_getattrib(S, G, FLUIDS_FLUX1);
   fluids_getattrib(S, &cs2, FLUIDS_SOUNDSPEEDSQUARED);
   fluids_del(S);
-  assert(F[0] == 1.0);
-  assert(F[1] == 5.0);
-  assert(F[2] == 2.0);
-  assert(G[0] == 1.0);
-  assert(G[1] == 5.0);
-  assert(G[2] == 1.0);
-  assert(cs2 == 1.4);
+  asserteq(F[0], 1.0);
+  asserteq(F[1], 5.0);
+  asserteq(F[2], 2.0);
+  asserteq(G[0], 1.0);
+  asserteq(G[1], 5.0);
+  asserteq(G[2], 1.0);
+  asserteq(cs2, 1.4);
   printf("TEST 2 PASSED\n");
   return 0;
 }
@@ -70,12 +73,12 @@ int test3()
   fluids_setattrib(S, &gam, FLUIDS_GAMMALAWINDEX);
   fluids_setattrib(S, x, FLUIDS_PRIMITIVE);
   fluids_p2c(S);
-  fluids_update(S, FLUIDS_FLUX0 | FLUIDS_EVALS1);
-  fluids_getattrib(S, lam1, FLUIDS_EVALS1);
+  fluids_update(S, FLUIDS_FLUX0 | FLUIDS_EVAL1);
+  fluids_getattrib(S, lam1, FLUIDS_EVAL1);
   fluids_getattrib(S, &cs2, FLUIDS_SOUNDSPEEDSQUARED);
   fluids_del(S);
-  assert(lam1[1] == 1.0);
-  assert(cs2 == 0.0);
+  asserteq(lam1[1], 1.0);
+  asserteq(cs2, 0.0);
   printf("TEST 3 PASSED\n");
   return 0;
 }
@@ -101,11 +104,11 @@ int test4()
   fluids_setattrib(S, x, FLUIDS_PRIMITIVE);
   fluids_p2c(S);
   fluids_update(S,
-		FLUIDS_EVALS0 |
-		FLUIDS_LEVECS0 |
-		FLUIDS_REVECS0 |
-		FLUIDS_JACOBIAN0);
-  fluids_getattrib(S, V, FLUIDS_EVALS0);
+                FLUIDS_EVAL0 |
+                FLUIDS_LEVECS0 |
+                FLUIDS_REVECS0 |
+                FLUIDS_JACOBIAN0);
+  fluids_getattrib(S, V, FLUIDS_EVAL0);
   fluids_getattrib(S, L, FLUIDS_LEVECS0);
   fluids_getattrib(S, R, FLUIDS_REVECS0);
   fluids_getattrib(S, A, FLUIDS_JACOBIAN0);
@@ -115,8 +118,8 @@ int test4()
   matrix_matrix_product(LA, R, LAR, 5, 5, 5);
   for (int m=0; m<5; ++m) {
     for (int n=0; n<5; ++n) {
-      assert(fabs(I[m*5 + n] - (m==n)) < 1e-12);
-      assert(fabs(LAR[m*5 + n] - (m==n) * V[m]) < 1e-12);
+      asserteq(I[m*5 + n], (m==n));
+      asserteq(LAR[m*5 + n], (m==n) * V[m]);
     }
   }
   printf("TEST 4 PASSED\n");
@@ -129,67 +132,48 @@ int test4()
 // -----------------------------------------------------------------------------
 int test5()
 {
-  double Pl[5] = {1, 1, 1, 1, 1};
-  double Pr[5] = {1, 1, 1, 1, 1};
-  double P_[5];
-  double gam = 1.4;
-  fluid_state *SL = fluids_new();
-  fluid_state *SR = fluids_new();
-  fluid_state *S_ = fluids_new();
+  int solvers[2] = {FLUIDS_RIEMANN_HLL, FLUIDS_RIEMANN_EXACT};
+  for (int solver=0; solver<2; ++solver) {
+    double Pl[5] = {1, 1, 1, 1, 1};
+    double Pr[5] = {1, 1, 1, 1, 1};
+    double P_[5];
+    double gam = 1.4;
+    fluid_state *SL = fluids_new();
+    fluid_state *SR = fluids_new();
+    fluid_state *S_ = fluids_new();
 
-  fluids_setfluid(SL, FLUIDS_NRHYD);
-  fluids_setfluid(SR, FLUIDS_NRHYD);
-  fluids_setfluid(S_, FLUIDS_NRHYD);
+    fluids_setfluid(SL, FLUIDS_NRHYD);
+    fluids_setfluid(SR, FLUIDS_NRHYD);
+    fluids_setfluid(S_, FLUIDS_NRHYD);
 
-  fluids_setattrib(SL, &gam, FLUIDS_GAMMALAWINDEX);
-  fluids_setattrib(SR, &gam, FLUIDS_GAMMALAWINDEX);
-  fluids_setattrib(S_, &gam, FLUIDS_GAMMALAWINDEX);
+    fluids_setattrib(SL, &gam, FLUIDS_GAMMALAWINDEX);
+    fluids_setattrib(SR, &gam, FLUIDS_GAMMALAWINDEX);
+    fluids_setattrib(S_, &gam, FLUIDS_GAMMALAWINDEX);
+    fluids_setattrib(SL, Pl, FLUIDS_PRIMITIVE);
+    fluids_setattrib(SR, Pr, FLUIDS_PRIMITIVE);
+    fluids_p2c(SL);
+    fluids_p2c(SR);
 
-  fluids_setattrib(SL, Pl, FLUIDS_PRIMITIVE);
-  fluids_setattrib(SR, Pr, FLUIDS_PRIMITIVE);
-
-  fluid_riemann *R = fluids_riemann_new();
-  fluids_riemann_setdim(R, 0);
-  fluids_riemann_setstateL(R, SL);
-  fluids_riemann_setstateR(R, SR);
-  fluids_riemann_execute(R);
-  fluids_riemann_sample(R, S_, 0.2);
-  fluids_getattrib(S_, P_, FLUIDS_PRIMITIVE);
-  for (int n=0; n<5; ++n) {
-    assert(P_[n] == Pl[n]);
-  }
-  printf("TEST 5 PASSED\n");
-
-  Pl[0] = 1.0;
-  Pl[1] = 1.0;
-  Pl[2] = 0.0;
-  Pl[3] = 0.0;
-  Pl[4] = 0.0;
-  Pr[0] = 0.1;
-  Pr[1] = 0.125;
-  Pr[2] = 0.0;
-  Pr[3] = 0.0;
-  Pr[4] = 0.0;
-
-  fluids_setattrib(SL, Pl, FLUIDS_PRIMITIVE);
-  fluids_setattrib(SR, Pr, FLUIDS_PRIMITIVE);
-  fluids_riemann_setstateL(R, SL);
-  fluids_riemann_setstateR(R, SR);
-  fluids_riemann_execute(R);
-
-  FILE *outf = fopen("riemann.dat", "w");
-  for (int n=0; n<4000; ++n) {
-    double x = -2.0 + 1e-3 * (n + 0.5);
-    fluids_riemann_sample(R, S_, x);
+    fluid_riemann *R = fluids_riemann_new();
+    fluids_riemann_setsolver(R, solvers[solver]);
+    fluids_riemann_setdim(R, 0);
+    fluids_riemann_setstateL(R, SL);
+    fluids_riemann_setstateR(R, SR);
+    fluids_riemann_execute(R);
+    fluids_riemann_sample(R, S_, 0.2);
+    fluids_c2p(S_);
     fluids_getattrib(S_, P_, FLUIDS_PRIMITIVE);
-    fprintf(outf, "%f %f %f %f %f %f\n", x, P_[0], P_[1], P_[2], P_[3], P_[4]);
-  }
-  fclose(outf);
 
-  fluids_riemann_del(R);
-  fluids_del(SL);
-  fluids_del(SR);
-  fluids_del(S_);
+    for (int n=0; n<5; ++n) {
+      asserteq(P_[n], Pl[n]);
+    }
+    printf("TEST 5.%d PASSED\n", solver);
+
+    fluids_riemann_del(R);
+    fluids_del(SL);
+    fluids_del(SR);
+    fluids_del(S_);
+  }
   return 0;
 }
 
