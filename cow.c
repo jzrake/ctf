253527dc07bd53c675842ea0e7177d302ba332a7
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-10-24 11:52:37 -0400

    Removes isinf and isnan functions

--- cow.c
+++ cow.c
@@ -12,29 +12,6 @@
 //
 // -----------------------------------------------------------------------------
 
-#ifndef isnan
-# define isnan(x)                                       \
-  (sizeof (x) == sizeof (long double) ? isnan_ld (x)    \
-   : sizeof (x) == sizeof (double) ? isnan_d (x)        \
-   : isnan_f (x))
-static inline int isnan_f  (float       x) { return x != x; }
-static inline int isnan_d  (double      x) { return x != x; }
-static inline int isnan_ld (long double x) { return x != x; }
-#endif
-
-#ifndef isinf
-# define isinf(x)                                       \
-  (sizeof (x) == sizeof (long double) ? isinf_ld (x)    \
-   : sizeof (x) == sizeof (double) ? isinf_d (x)        \
-   : isinf_f (x))
-static inline int isinf_f  (float       x)
-{ return !isnan (x) && isnan (x - x); }
-static inline int isinf_d  (double      x)
-{ return !isnan (x) && isnan (x - x); }
-static inline int isinf_ld (long double x)
-{ return !isnan (x) && isnan (x - x); }
-#endif
-
 #if (COW_MPI)
 static void _domain_maketags1d(cow_domain *d);
 static void _domain_maketags2d(cow_domain *d);
