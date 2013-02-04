0abc33395fe3890b58efba5d3c51d84af55baf1c
Jonathan Zrake <jonathan.zrake@gmail.com>
2013-02-04 18:36:08 -0500

    Make increment toward advancing many blocks

--- examples/smr1d.lua
+++ examples/smr1d.lua
@@ -48,15 +48,23 @@ function StaticMeshRefinement:initialize_solver()
    fluids.descr_setgamma(descr, 1.4)
    fluids.descr_seteos(descr, fluids.EOS_GAMMALAW)
 
-   local block = fish.block_new()
-   fish.block_setdescr(block, descr)
-   fish.block_setrank(block, 1)
-   fish.block_setsize(block, 0, self.N)
-   fish.block_setrange(block, 0, 0.0, 1.0)
-   fish.block_setguard(block, self.Ng)
-   fish.block_allocate(block)
-   fish.block_setneighbor(block, 0, fish.LEFT, block)
-   fish.block_setneighbor(block, 0, fish.RIGHT, block)
+   local blocks = { }
+   for i=1,1 do
+      local block = fish.block_new()
+      fish.block_setdescr(block, descr)
+      fish.block_setrank(block, 1)
+      fish.block_setsize(block, 0, self.N)
+      fish.block_setrange(block, 0, 0.0, 1.0)
+      fish.block_setguard(block, self.Ng)
+      fish.block_allocate(block)
+      blocks[i] = block
+   end
+   for i=1,1 do
+      local BL = blocks[i-1] or blocks[1]
+      local BR = blocks[i+1] or blocks[1]
+      fish.block_setneighbor(blocks[i], 0, fish.LEFT, BL)
+      fish.block_setneighbor(blocks[i], 0, fish.RIGHT, BR)
+   end
 
    local scheme = fish.state_new()
    fish.setparami(scheme, fluids[RS], fish.RIEMANN_SOLVER)
@@ -67,8 +75,9 @@ function StaticMeshRefinement:initialize_solver()
    fish.setparamd(scheme, 2.0, fish.PLM_THETA)
 
    self.descr = descr
-   self.block = block
+   self.blocks = blocks
    self.scheme = scheme
+   self.primitive = { }
 end
 
 function StaticMeshRefinement:report_configuration()
@@ -100,19 +109,29 @@ function StaticMeshRefinement:report_configuration()
 end
 
 function StaticMeshRefinement:finalize_solver()
-   fish.block_del(self.block)
+   for _,block in pairs(self.blocks) do
+      fish.block_del(self.block)
+   end
    fish.state_del(self.scheme)
    fluids.descr_del(self.descr)
 end
 
 function StaticMeshRefinement:initialize_physics()
-   local P, G = self.problem:solution(0.0)
-   fish.block_mapbuffer(self.block, P:buffer(), fluids.PRIMITIVE)
-   self.Primitive = P
+   for _,block in pairs(self.blocks) do
+      local P = self.problem:solution(0.0)
+      fish.block_mapbuffer(block, P:buffer(), fluids.PRIMITIVE)
+      self.primitive[block] = P
+   end
 end
 
 function StaticMeshRefinement:set_time_increment()
-   local Amax = fish.block_maxwavespeed(self.block)
+   local Amax = 0.0
+   for _,block in pairs(self.blocks) do
+      local A = fish.block_maxwavespeed(block)
+      if A > Amax then
+	 Amax = A
+      end
+   end
    local dt = self.CFL * self.dx / Amax
    self.status.time_increment = dt
 end
@@ -121,62 +140,85 @@ function StaticMeshRefinement:advance_physics()
 
    local dt = self.status.time_increment
    local enum = array.vector(1, 'int')
+   local block = self.blocks[1]
    fish.getparami(self.scheme, enum:buffer(), fish.TIME_UPDATE)
 
    if enum[0] == fish.SINGLE then
-      local W = array.vector{1.0, 0.0, 1.0}
+      local W0 = array.vector{1.0, 0.0, 1.0}
 
-      fish.block_fillconserved(self.block)
-      fish.block_timederivative(self.block, self.scheme)
-      fish.block_evolve(self.block, W:buffer(), dt)
-      fish.block_fillguard(self.block)
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillconserved(block)
+      end
+      -- ****************************** Step 1 ****************************** --
+      for _,block in pairs(self.blocks) do
+	 fish.block_timederivative(block, self.scheme)
+	 fish.block_evolve(block, W0:buffer(), dt)
+      end
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillguard(block)
+      end
 
    elseif enum[0] == fish.MIDPOINT then
       local W0 = array.vector{1.0, 0.0, 0.5}
       local W1 = array.vector{1.0, 0.0, 1.0}
 
-      fish.block_fillconserved(self.block)
-
-      fish.block_timederivative(self.block, self.scheme)
-      fish.block_evolve(self.block, W0:buffer(), dt)
-      fish.block_fillguard(self.block)
-
-      fish.block_timederivative(self.block, self.scheme)
-      fish.block_evolve(self.block, W1:buffer(), dt)
-      fish.block_fillguard(self.block)
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillconserved(block)
+      end
+      -- ****************************** Step 1 ****************************** --
+      for _,block in pairs(self.blocks) do
+	 fish.block_timederivative(block, self.scheme)
+	 fish.block_evolve(block, W0:buffer(), dt)
+      end
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillguard(block)
+      end
+      -- ****************************** Step 2 ****************************** --
+      for _,block in pairs(self.blocks) do
+	 fish.block_timederivative(block, self.scheme)
+	 fish.block_evolve(block, W1:buffer(), dt)
+      end
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillguard(block)
+      end
 
    elseif enum[0] == fish.SHUOSHER_RK3 then
       local W0 = array.vector{1.0, 0.0, 1.0}
       local W1 = array.vector{3/4, 1/4, 1/4}
       local W2 = array.vector{1/3, 2/3, 2/3}
 
-      fish.block_fillconserved(self.block)
-
-      fish.block_timederivative(self.block, self.scheme)
-      fish.block_evolve(self.block, W0:buffer(), dt)
-      fish.block_fillguard(self.block)
-
-      fish.block_timederivative(self.block, self.scheme)
-      fish.block_evolve(self.block, W1:buffer(), dt)
-      fish.block_fillguard(self.block)
-
-      fish.block_timederivative(self.block, self.scheme)
-      fish.block_evolve(self.block, W2:buffer(), dt)
-      fish.block_fillguard(self.block)
-   else
-      error('unrecognized option: advance='..FishEnums[enum[0]])
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillconserved(block)
+      end
+      -- ****************************** Step 1 ****************************** --
+      for _,block in pairs(self.blocks) do
+	 fish.block_timederivative(block, self.scheme)
+	 fish.block_evolve(block, W0:buffer(), dt)
+      end
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillguard(block)
+      end
+      -- ****************************** Step 2 ****************************** --
+      for _,block in pairs(self.blocks) do
+	 fish.block_timederivative(block, self.scheme)
+	 fish.block_evolve(block, W1:buffer(), dt)
+      end
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillguard(block)
+      end
+      -- ****************************** Step 3 ****************************** --
+      for _,block in pairs(self.blocks) do
+	 fish.block_timederivative(block, self.scheme)
+	 fish.block_evolve(block, W2:buffer(), dt)
+      end
+      for _,block in pairs(self.blocks) do
+	 fish.block_fillguard(block)
+      end
    end
 end
 
 function StaticMeshRefinement:checkpoint_write()
-   local n = self.status.checkpoint_number
-   local t = self.status.simulation_time
-   local Ng = self.Ng
-   local fname = string.format('data/chkpt.%04d.h5', n)
-   local outfile = hdf5.File(fname, 'w')
-   outfile['prim' ] = self.Primitive          [{{Ng,-Ng},nil}]
-   outfile['exact'] = self.problem:solution(t)[{{Ng,-Ng},nil}]
-   outfile:close()
+
 end
 
 function StaticMeshRefinement:local_mesh_size()
@@ -190,7 +232,7 @@ end
 function StaticMeshRefinement:user_work_finish()
    local t = self.status.simulation_time
    local Ng = self.Ng
-   local P = self.Primitive
+   local P = self.primitive[self.blocks[1]]
    local Pexact = self.problem:solution(t)
    local dx = self.dx
    local P0 = Pexact[{{Ng,-Ng},nil}]:vector()
@@ -226,7 +268,7 @@ local opts = {plot=true,
 	      tmax=0.2,
 	      solver='godunov',
 	      reconstruction='weno5',
-	      advance='midpoint'}
+	      advance='rk3'}
 local sim = StaticMeshRefinement(opts)
 local problem = problems.densitywave(opts)
 sim:run(problem)
