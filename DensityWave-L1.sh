bce29a7d6e00dcf82f17d1a8012515b229e963da
Jonathan Zrake <jonathan.zrake@gmail.com>
2013-02-13 18:40:08 -0500

    Add a bunch of documentation data and markup, get ready for thesis work

---
+++ DensityWave-L1.sh
@@ -0,0 +1,52 @@
+
+# ----------------------------------------------------------
+# Mara: HLLC-PLM-MUSCL
+# ----------------------------------------------------------
+./ctf examples/tests-1d.lua densitywave --convergence \
+    --backend=mara \
+    --tmax=0.5 \
+    --solver=muscl \
+    --advance=single \
+    --output=hllc-plm-muscl.dat
+
+# ----------------------------------------------------------
+# Mara: HLLC-PLM-RK3
+# ----------------------------------------------------------
+./ctf examples/tests-1d.lua densitywave --convergence \
+    --backend=mara \
+    --tmax=0.5 \
+    --solver=godunov \
+    --advance=rk3 \
+    --output=hllc-plm-rk3.dat
+
+# ----------------------------------------------------------
+# Fish: HLLC-WENO5-RK3
+# ----------------------------------------------------------
+./ctf examples/tests-1d.lua densitywave --convergence \
+    --backend=fish \
+    --tmax=0.5 \
+    --reconstruction=weno5 \
+    --solver=godunov \
+    --advance=rk3 \
+    --output=hllc-weno5-rk3.dat
+
+# ----------------------------------------------------------
+# Mara: CHAR-WENO5-RK3
+# ----------------------------------------------------------
+./ctf examples/tests-1d.lua densitywave --convergence \
+    --backend=mara \
+    --tmax=0.5 \
+    --solver=spectral \
+    --advance=rk3 \
+    --output=char-weno5-rk3.dat
+
+# ----------------------------------------------------------
+# Mara: CHAR-WENO5-RK4
+# ----------------------------------------------------------
+./ctf examples/tests-1d.lua densitywave --convergence \
+    --backend=mara \
+    --tmax=0.5 \
+    --solver=spectral \
+    --advance=rk4 \
+    --output=char-weno5-rk4.dat
+
